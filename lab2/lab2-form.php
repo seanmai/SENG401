4cde00c38695f24afa33a2be2db69c82d85867bf
Sean Mai <seandmai@gmail.com>
2019-02-12 22:45:47 -0700

    Lab2 pre-ajax

--- lab2/lab2-form.php
+++ lab2/lab2-form.php
@@ -8,16 +8,17 @@ if((!empty($latitude1) && is_real($latitude1) && !empty($longitude1) && is_real(
         $quadrant1 = getQuadrant($latitude1, $longitude1);
         $quadrant2 = getQuadrant($latitude2, $longitude2);
         $bearing = calculateBearing($latitude1, $longitude1, $latitude2, $longitude2);
+        $distance = calculateDistance($latitude1, $longitude1, $latitude2, $longitude2);
         echo "Point 1 Quadrant: $quadrant1 </br>";
         echo "Point 2 Quadrant: $quadrant2 </br>";
-        echo "Bearing angle between points is : $bearing </br>";
+        echo "Bearing angle between points is : $bearing degrees </br>";
+        echo "Great circle distance between points is : $distance km</br>";
     } else {
         echo "Coordinates not in range";
     }
 }
 else{
     echo "Please enter valid inputs.";
-
 }
 
 function getQuadrant(float $lat, float $lon){
@@ -43,5 +44,13 @@ function calculateBearing(float $lat1, float $lon1, float $lat2, float $lon2){
     }
     if($theta < 0) $theta += 180;
     return $theta;
+}
+
+function calculateDistance(float $lat1, float $lon1, float $lat2, float $lon2){
+    $dlon = $lon2 - $lon1;
+    $dlat = $lat2 - $lat1;
+    $a = (pow(sin($dlat/2), 2) + cos($lat1) * cos($lat2) * pow(sin($dlon/2), 2));
+    $c = (2 * asin(min(1, sqrt($a))));
+    return ($a * $c);
 }
 ?>
